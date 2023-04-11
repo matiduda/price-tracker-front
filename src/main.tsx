856ea0084e2c8d8a1713167043e9429d62ff5950
Mateusz Duda <mateusz.duda2@gmail.com>
2023-04-11 17:38:05 +0200

    added back fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Fonts from "./fonts/Fonts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Fonts />
     <App />
   </React.StrictMode>
 );
