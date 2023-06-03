3f3dbb7e4dbf43fdac5c612ef32c4a0c126c6f78
Mateusz Duda <mateusz.duda2@gmail.com>
2023-06-03 23:24:41 +0200

    refined subscription page

--- src/routes/subscription/Subscription.tsx
+++ src/routes/subscription/Subscription.tsx
@@ -1,51 +1,46 @@
 import { ReactElement } from "react";
 import { WelcomeBox } from "../../components/LandingInfo/WelcomeBox";
-import { SimpleGrid, Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, Flex, List, ListItem, ListIcon } from "@chakra-ui/react";
+import { SimpleGrid, Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, Flex, List, ListItem, ListIcon, Box } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
+import SubscriptionInfo from "../../components/SubscriptionInfo/SubscriptionInfo";
 
-
-const CARD_WIDTH = 300;
+export enum SubscriptionType {
+  ONE_MONTH = 0,
+  THREE_MONTHS = 1,
+  TWELVE_MONTHS = 2,
+}
 
 const Subscription = (): ReactElement => {
   return (
     <>
+      <Box height="200px" />
+      <Text textAlign={"center"} mb={10} sx={{
+        fontWeight: "bold",
+        fontSize: "30px"
+      }}>
+        Select you plan
+      </Text>
       <Flex width="100%" justifyContent="center" flexWrap="wrap">
-
-        <SimpleGrid columns={4} spacing={5}>
-          <Card maxW={CARD_WIDTH}>
-            <CardBody>
-              <Text textAlign={"center"} sx={{
-                fontWeight: "bold",
-                fontSize: "20px"
-              }}>Standard</Text>
-              <Text textAlign={"center"} sx={{
-                fontWeight: "bold",
-                fontSize: "30px"
-              }} my={3}>4.99 $</Text>
-              <Text mb={2}>Our most basic plan which includes:</Text>
-              <List spacing={3}>
-                <ListItem>
-                  <ListIcon as={CheckCircleIcon} color='green.500' />
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={CheckCircleIcon} color='green.500' />
-                  Assumenda, quia temporibus eveniet a libero incidunt suscipit
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={CheckCircleIcon} color='green.500' />
-                  Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                </ListItem>
-              </List>
-            </CardBody>
-            <CardFooter>
-              <Flex justifyContent={"center"} width="100%">
-                <Button>Select</Button>
-              </Flex>
-            </CardFooter>
-          </Card>
+        <SimpleGrid columns={3} spacing={10}>
+          <SubscriptionInfo price={4.99} months={1} type={SubscriptionType.ONE_MONTH} heading={"1 month"}>
+            <Text>
+              Our most basic plan which includes basic price tracking
+            </Text>
+          </SubscriptionInfo>
+          <SubscriptionInfo price={11.97} months={3} type={SubscriptionType.THREE_MONTHS} heading={"3 months"}>
+            <Text>
+              Our most basic plan which includes basic price tracking
+            </Text>
+          </SubscriptionInfo>
+          <SubscriptionInfo price={35.88} months={12} type={SubscriptionType.ONE_MONTH} heading={"One year"}>
+            <Text>
+              Our most basic plan which includes basic price tracking
+            </Text>
+          </SubscriptionInfo>
         </SimpleGrid>
       </Flex>
+      <Box height="200px" />
+
     </>
   );
 };
